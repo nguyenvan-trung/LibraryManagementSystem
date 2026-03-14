28fb2a5b49bd30e29e82acde1ee89535a21ed9f8
nguyenvan-trung <nguyenvantrungtn4510273@gmail.com>
2026-03-14 21:32:39 +0700

    feat: here write screen for login and register

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class BooksController: Controller
+    {
+        private readonly LibraryDbContext context = new LibraryDbContext();
+        public ActionResult Index()
+        {
+            var books = context.Books.ToList();
+            return View(books);
+        }
+    }
+}
--- Models/Student.cs
+++ Models/Student.cs
@@ -14,7 +14,7 @@ namespace WebApplication1.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public string Email { get; set; }
-
+        public string Password { get; set; }
         public int RoleId { get; set; }
 
         public virtual Role Role { get; set; }
